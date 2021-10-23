7b10dc5f003396fcb2671532a9ebe200e6e1893b
Hojun Bun <ponnponn.p@gmail.com>
2021-10-24 00:28:05 +0900

    add post css and enable css modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,12 @@
 const path = require("path");
 module.exports = {
   stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss",
+    "storybook-css-modules-preset",
+  ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
